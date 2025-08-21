a358354a1ee5bc82453dc47bdc734944fd2b211e
Roopam404 <kulkarniroopam@gmail.com>
2025-08-21 20:15:40 +0530

    Create Print Name Class and Age.cpp

---
+++ Basic/Print Name Class and Age.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()                   
+{
+	cout<<"My name is Roopam Kulkarni"<<endl;
+    cout<<"Im in SY B.Tech Robotics"<<endl;
+    cout<<"Im 19 years old"<<endl;
+    return 0;
+}
