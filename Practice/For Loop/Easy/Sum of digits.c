20b810efd2982086deabf66416fb82f79af325e6
Roopam404 <kulkarniroopam@gmail.com>
2025-09-29 23:43:43 +0530

    Create Sum of digits.c

---
+++ Practice/For Loop/Easy/Sum of digits.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n,sum=0;
+  
+    printf("Enter number: ");
+    scanf("%d",&n);
+  
+    for(;n>0;n/=10) 
+      sum+=n%10;
+      printf("Sum of digits=%d",sum);
+  
+    return 0;
+}
