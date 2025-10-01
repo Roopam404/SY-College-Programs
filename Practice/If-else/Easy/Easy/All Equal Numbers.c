52228796e7b538fa5e6c56b53e12c558c8e8bb8f
Roopam404 <kulkarniroopam@gmail.com>
2025-10-01 21:58:32 +0530

    Create All Equal Numbers.c

---
+++ Practice/If-else/Easy/Easy/All Equal Numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a, b, c;
+  
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a == b && b == c)
+        printf("All numbers are equal\n");
+    else
+        printf("Numbers are not equal\n");
+
+    return 0;
+}
