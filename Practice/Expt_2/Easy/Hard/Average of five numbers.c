d6a9560123b77e391718a9064e4ff6633fc5a050
Roopam404 <kulkarniroopam@gmail.com>
2025-10-01 23:45:18 +0530

    Create Average of five numbers.c

---
+++ Practice/Expt_2/Easy/Hard/Average of five numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() 
+{
+    float a, b, c, d, e, avg;
+  
+    printf("Enter five numbers: ");
+    scanf("%f %f %f %f %f", &a, &b, &c, &d, &e);
+  
+    avg = (a + b + c + d + e) / 5;
+    printf("Average = %.2f\n", avg);
+  
+    return 0;
+}
