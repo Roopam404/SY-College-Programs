d56469c7c62f3a7b8b2c55c9a9b7cf2a3a7bb39d
Roopam404 <kulkarniroopam@gmail.com>
2025-10-01 21:32:20 +0530

    Create Find the smallest digit in a number.c

---
+++ Practice/For Loop/Medium/Find the smallest digit in a number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() 
+{
+    int n, min=9;
+  
+    printf("Enter number: ");
+    scanf("%d",&n);
+  
+    for(; n!=0; n/=10){
+        int d = n%10;
+        if(d < min) min = d;
+    }
+  
+    printf("Smallest digit = %d", min);
+  
+    return 0;
+}
