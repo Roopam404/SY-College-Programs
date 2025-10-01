ba7cd4c5a614005f203483f72337c172e0b57e2c
Roopam404 <kulkarniroopam@gmail.com>
2025-10-01 23:21:13 +0530

    Create Sandglass pattern of numbers.c

---
+++ Practice/For Loop/Hard/Sandglass pattern of numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() 
+{
+    int n;
+  
+    printf("Enter n: ");
+    scanf("%d",&n);
+  
+    for(int i=n;i>=1;i--){
+        for(int j=n;j>i;j--) printf(" ");
+        for(int j=1;j<=2*i-1;j++) printf("%d", j);
+        printf("\n");
+    }
+    for(int i=2;i<=n;i++){
+        for(int j=i;j<n;j++) printf(" ");
+        for(int j=1;j<=2*i-1;j++) printf("%d", j);
+        printf("\n");
+    }
+    return 0;
+}
