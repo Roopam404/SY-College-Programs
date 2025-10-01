cb89cc2b6cbdedb361d6f16a17bd4e25bdf08fd6
Roopam404 <kulkarniroopam@gmail.com>
2025-10-01 23:07:10 +0530

    Delete Easy directory

--- Easy/Add two numbers.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main() {
-    int a = 5, b = 3;
-    printf("Sum = %d\n", a + b);
-    return 0;
-}
--- Easy/Print Name.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main() {
-    printf("My name is Roopam\n");
-    return 0;
-}
