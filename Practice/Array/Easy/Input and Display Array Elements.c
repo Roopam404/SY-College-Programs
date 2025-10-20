fa6304726a060b3949637c6617d0a8cde8ead28a
Roopam404 <kulkarniroopam@gmail.com>
2025-10-20 15:58:42 +0530

    Create Input and Display Array Elements.c

---
+++ Practice/Array/Easy/Input and Display Array Elements.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() 
+{
+    int a[5], i;
+    printf("Enter 5 numbers: ");
+    for(i=0; i<5; i++)
+        scanf("%d", &a[i]);
+    printf("You entered: ");
+    for(i=0; i<5; i++)
+        printf("%d ", a[i]);
+    return 0;
+}
