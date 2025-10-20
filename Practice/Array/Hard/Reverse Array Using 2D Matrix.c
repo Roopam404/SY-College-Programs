057b19af90c3bc461a35b8094b71728072d651f2
Roopam404 <kulkarniroopam@gmail.com>
2025-10-20 16:25:06 +0530

    Create Reverse Array Using 2D Matrix.c

---
+++ Practice/Array/Hard/Reverse Array Using 2D Matrix.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a[2][3], i,j;
+    for(i=0;i<2;i++) for(j=0;j<3;j++) scanf("%d",&a[i][j]);
+    printf("Reversed row-wise:\n");
+    for(i=0;i<2;i++){
+        for(j=2;j>=0;j--) printf("%d ",a[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
