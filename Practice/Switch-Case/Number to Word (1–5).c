709bdf1bb58bd7b5472775242396f574c06b727c
Roopam404 <kulkarniroopam@gmail.com>
2025-10-20 15:36:42 +0530

    Create Number to Word (1–5).c

---
+++ Practice/Switch-Case/Number to Word (1–5).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter number (1–5): ");
+    scanf("%d", &n);
+
+    switch(n) {
+        case 1: printf("One"); break;
+        case 2: printf("Two"); break;
+        case 3: printf("Three"); break;
+        case 4: printf("Four"); break;
+        case 5: printf("Five"); break;
+        default: printf("Out of range");
+    }
+    return 0;
+}
